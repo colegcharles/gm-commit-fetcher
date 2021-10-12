567119d453364f8edb8e8422f5605892d430c6a0
cole <coleybear1010@gmail.com>
2021-10-12 16:39:48 -0400

    Initial Commit

---
+++ gm-commit-fetcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gm-commit-fetcher
+//
+//  Created by Cole Charles on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
